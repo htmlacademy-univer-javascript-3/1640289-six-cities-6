5d9d3938bd8031cb230c6e81b83befb5e6b09ba4
Станислав Зиганшин <stanislavzgn@gmail.com>
2025-11-01 19:07:21 +0300

    feat: add pages and card component for main page

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+const inputData = {
+  placesCount: 5,
+};
 
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <App placesCount={inputData.placesCount} />
   </React.StrictMode>
 );
